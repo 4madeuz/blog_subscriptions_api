164badec95f779b8ca1c08ee7104eb3c9af00820
4madeuz <84397194+4madeuz@users.noreply.github.com>
2024-02-16 17:35:33 +0500

    posts  subscription permissions

--- app/blog/api/v1/views.py
+++ app/blog/api/v1/views.py
@@ -1,10 +1,29 @@
 from blog.api.v1.serializers import PostSerializer, SubscriptionSerializer
 from rest_framework import status, viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from blog.models import Post, Subscription
+from blog.permissions import IsOwnerOrAdminPermission
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def get_permissions(self):
+        if self.action == 'get':
+            return [AllowAny()]
+        elif self.action == 'create':
+            return [IsAuthenticated()]
+        else:
+            return [IsOwnerOrAdminPermission()]
 
-class SubscriptionViewSet(viewsets.ModelViewSet):
+
+class SubscriptionViewSet(viewsets.mixins.CreateModelMixin,
+                          viewsets.mixins.DestroyModelMixin,
+                          viewsets.mixins.ListModelMixin,
+                          viewsets.GenericViewSet):
+    queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+
+    def get_queryset(self):
+        return Subscription.objects.filter(subscriber=self.request.user)
